c42e68a3749df6aaa7787c5e0aca59230b321202
Jorge Ivan Canavati <ie707634@iteso.mx>
2022-04-24 14:41:51 -0700

    Se agrego el front de favoritos

--- lib/songs/favorites.dart
+++ lib/songs/favorites.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:practica2/homePage/homePage.dart';
+import 'package:practica2/songs/bloc/song_bloc.dart';
 
 class Favorites extends StatefulWidget {
   Favorites({Key? key}) : super(key: key);
@@ -10,6 +13,81 @@ class Favorites extends StatefulWidget {
 class _FavoritesState extends State<Favorites> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return BlocConsumer<SongBloc, SongState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => homePage()));
+              },
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: [
+                            Card(
+                              semanticContainer: true,
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              child: Image.network(
+                                'https://placeimg.com/640/480/any',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Stack(
+                              children: [
+                                Center(
+                                  child: SizedBox(
+                                    height: 50,
+                                    width:
+                                        MediaQuery.of(context).size.width - 9,
+                                    child: const DecoratedBox(
+                                      decoration: const BoxDecoration(
+                                          color: Colors.indigo),
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text("Titulo",
+                                          style: TextStyle(
+                                              fontSize: 25,
+                                              fontWeight: FontWeight.bold)),
+                                      Text("Artista"),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
